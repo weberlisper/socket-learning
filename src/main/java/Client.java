7c0a793b7a741c6f9cb145074e12fd525fc802b5
lsp <317118524@qq.com>
2021-01-16 23:43:12 +0800

    使用socket发送Java基础数据：ByteBuffer

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,13 +1,17 @@
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
 
 public class Client {
+    private static final int PORT = 2000;
+    private static final int LOCAL_PORT = 2001;
 
     public static void main(String[] args) throws IOException {
-        Socket client = new Socket();
-        client.setSoTimeout(3000);
+        Socket client = createSocket();
 
-        client.connect(new InetSocketAddress(InetAddress.getLocalHost(), 2000), 3000);
+        initSocket(client);
+
+        client.connect(new InetSocketAddress(InetAddress.getLocalHost(), PORT), 3000);
 
         System.out.println("客户端已连接到服务器：" + client.getInetAddress() + "/" + client.getPort());
 
@@ -21,32 +25,64 @@ public class Client {
         System.out.println("与服务器：" + client.getInetAddress() + "/" + client.getPort() + "的连接已断开");
     }
 
+    private static void initSocket(Socket socket) throws SocketException {
+        socket.setSoTimeout(3000);
+    }
+
+    private static Socket createSocket() throws IOException {
+        Socket socket = new Socket();
+        socket.bind(new InetSocketAddress(InetAddress.getLocalHost(), LOCAL_PORT));
+        return socket;
+    }
+
     private static void todo(Socket client) throws IOException {
-        // 获取键盘输入流
-        InputStream in = System.in;
-        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+        // 获取socket输入输出流
+        OutputStream outputStream = client.getOutputStream();
+        InputStream inputStream = client.getInputStream();
+        byte[] buffer = new byte[256];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
 
-        // 获取socket输出流
-        PrintStream socketOutput = new PrintStream(client.getOutputStream());
+        // byte
+        byteBuffer.put((byte) 0x126);
 
-        // 获取socket输入流
-        BufferedReader socketInput = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        // char
+        byteBuffer.putChar('a');
 
+        // short
+        byteBuffer.putShort((short) 1234);
+
+        // int
+        byteBuffer.putInt(123456);
+
+        // long
+        byteBuffer.putLong(128302348784989533L);
+
+        // bool
         boolean flag = true;
-        do {
-            String writeLine = input.readLine();
-            socketOutput.println(writeLine);
+        byteBuffer.put((byte) (flag ? 1 : 0));
 
-            String readLine = socketInput.readLine();
-            if (readLine.equalsIgnoreCase("bye")) {
-                flag = false;
-            } else {
-                System.out.println(readLine);
-            }
-        } while (flag);
+        // float
+        byteBuffer.putFloat(128.1892f);
 
-        socketOutput.close();
-        socketInput.close();
+        // double
+        byteBuffer.putDouble(182.29384729384982);
+
+        // string
+        byteBuffer.put("Hello World!".getBytes());
+
+        outputStream.write(buffer, 0, byteBuffer.position() + 1);
+
+        // 读取数据
+        byte[] readBuffer = new byte[128];
+        int readCount = inputStream.read(readBuffer);
+        if (readCount > 0) {
+            System.out.println("收到数据：" + readBuffer[0]);
+        } else {
+            System.out.println("未收到数据: " + readCount);
+        }
+
+        outputStream.close();
+        inputStream.close();
     }
 
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,20 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(2000);
+        ServerSocket server = createServerSocket();
+
+        server.bind(new InetSocketAddress(InetAddress.getLocalHost(), 2000));
 
         for (; ; ) {
             Socket client = server.accept();
             System.out.println("连接客户端：" + client.getInetAddress() + "/" + client.getPort());
             new ClientHandler(client).start();
         }
+    }
+
+    private static ServerSocket createServerSocket() throws IOException {
+        return new ServerSocket();
     }
 
     private static class ClientHandler extends Thread {
@@ -27,26 +34,48 @@ public class Server {
         @Override
         public void run() {
             try {
-                // 获取socket输出流
-                PrintStream socketOutput = new PrintStream(client.getOutputStream());
+                // 获取socket输入输出流
+                OutputStream outputStream = client.getOutputStream();
+                InputStream inputStream = client.getInputStream();
+                byte[] buffer = new byte[256];
+                ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+                int readCount = inputStream.read(buffer);
+                if (readCount > 0) {
+                    // byte
+                    System.out.println(byteBuffer.get());
 
-                // 获取socket输入流
-                BufferedReader socketInput = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    // char
+                    System.out.println(byteBuffer.getChar());
 
-                boolean flag = true;
-                do {
-                    String readLine = socketInput.readLine();
-                    if (readLine.equalsIgnoreCase("bye")) {
-                        socketOutput.println("bye");
-                        flag = false;
-                    } else {
-                        System.out.println(readLine);
-                        socketOutput.println("回送：" + readLine.length());
-                    }
-                } while (flag);
+                    // short
+                    System.out.println(byteBuffer.getShort());
 
-                socketOutput.close();
-                socketInput.close();
+                    // int
+                    System.out.println(byteBuffer.getInt());
+
+                    // long
+                    System.out.println(byteBuffer.getLong());
+
+                    // bool
+                    System.out.println(byteBuffer.get() == 1);
+
+                    // float
+                    System.out.println(byteBuffer.getFloat());
+
+                    // double
+                    System.out.println(byteBuffer.getDouble());
+
+                    // string
+                    int position = byteBuffer.position();
+                    System.out.println(new String(buffer, position, buffer.length - position - 1));
+
+                } else {
+                    System.out.println("未收到数据: " + readCount);
+                }
+                outputStream.write(readCount);
+
+                outputStream.close();
+                inputStream.close();
             } catch (IOException e) {
                 System.out.println("客户端：" + client.getInetAddress() + "/" + client.getPort() + "异常断开");
             } finally {
